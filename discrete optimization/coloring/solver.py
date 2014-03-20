b34d38a020a2c730837594867569bb48fe69be72
sky <diufanshu@gmail.com>
2014-03-20 21:51:20 +0800

    just copy and my dirty works

---
+++ discrete optimization/coloring/solver.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+def solve_it(input_data):
+    # Modify this code to run your optimization algorithm
+
+    # parse the input
+    lines = input_data.split('\n')
+
+    first_line = lines[0].split()
+    node_count = int(first_line[0])
+    edge_count = int(first_line[1])
+
+    edges = []
+    for i in range(1, edge_count + 1):
+        line = lines[i]
+        parts = line.split()
+        edges.append((int(parts[0]), int(parts[1])))
+
+    # build a trivial solution
+    # every node has its own color
+#    solution = range(0, node_count)
+
+    # get all the nodes' neighbors:
+    neighbors = []
+    for i in xrange(node_count):
+    	temp = []
+    	for k in edges:
+    		if i in k:
+    			temp.append(k[1-k.index(i)])
+    	neighbors.append(temp)
+
+
+    # coloring...
+    solution = [0]*node_count
+    k = 0
+    for node in neighbors:
+        colors = []
+        for color in node:
+            colors.append(solution[color])
+            m = max(colors) + 2
+        if solution[k] in colors:
+            t = list(set(xrange(m))-set(colors))
+            t.sort()
+            solution[k] = t[0]            
+            
+        k += 1
+
+    node_count = max(solution)+1
+
+
+
+
+
+    # prepare the solution in the specified output format
+    output_data = str(node_count) + ' ' + str(0) + '\n'
+    output_data += ' '.join(map(str, solution))
+
+    return output_data
+
+
+import sys
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        file_location = sys.argv[1].strip()
+        input_data_file = open(file_location, 'r')
+        input_data = ''.join(input_data_file.readlines())
+        input_data_file.close()
+        print solve_it(input_data)
+    else:
+        print 'This test requires an input file.  Please select one from the data directory. (i.e. python solver.py ./data/gc_4_1)'
+
